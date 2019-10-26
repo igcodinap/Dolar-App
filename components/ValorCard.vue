fa834055eb5aee4311dff43c94bf823540f73159
igcodinap <igcodinap@gmail.com>
2019-10-26 14:01:28 -0700

    Initial FrontEnd Configuration

---
+++ components/ValorCard.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div>
+    <h3>{{valor.valor}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ValorCard',
+  props: {
+    valor: Object,
+  }
+}
+</script>
+<style lang ="css>"
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template id="">
+  <div class="container">
+    <h1 class="title">Valores dolar</h1>
+    <div class="columns"
+      <ValorCard :valor="valor" v-for="valor in valores" :key="valor.id"/>
+  </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import env from '../config/env';
+import ValorCard from '../components/ValorCard'
+  export default {
+    name: 'IndexPage',
+    components: {
+      ValorCard
+    },
+    data(){
+      return {
+        valores: [],
+      }
+    },
+    created(){
+      axios.get(`${env.endpoint}`).then(response=>{
+        this.valores = response.data;
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
